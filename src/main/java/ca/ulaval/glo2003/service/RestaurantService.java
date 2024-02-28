eae9a4b593fd0ea1d36e3570a329aaec3d5a1e0a
Willy juvenal Konlack foning <willy-juvenal.konlack-foning.1@ulaval.ca>
2024-02-28 05:07:58 -0500

    fix: refactor et clean code dans  RestaurantService
    -permettre de retourner un booléen sur les méthodes verifyRestaurantParameter et  isValidOwnerId
    -modification du constructeur pour prendre le repository en parametre
    -getRestaurant throws une NotFoundException  lorsque  le restaurant est pas trouve/
    
    #48

--- src/main/java/ca/ulaval/glo2003/service/RestaurantService.java
+++ src/main/java/ca/ulaval/glo2003/service/RestaurantService.java
@@ -3,6 +3,7 @@ package ca.ulaval.glo2003.service;
 import ca.ulaval.glo2003.entity.*;
 import ca.ulaval.glo2003.exception.InvalidParameterException;
 import ca.ulaval.glo2003.exception.MissingParameterException;
+import ca.ulaval.glo2003.exception.NotFoundException;
 import ca.ulaval.glo2003.repository.*;
 import ca.ulaval.glo2003.util.Validator;
 import java.util.List;
@@ -12,20 +13,26 @@ public class RestaurantService {
   private final RestaurantRespository restaurantRepository;
   private final Validator validator;
 
-  public RestaurantService() {
-    this.restaurantRepository = new RestaurantRespository();
+  public RestaurantService(RestaurantRespository restaurantRepository) {
+    this.restaurantRepository = restaurantRepository;
     this.validator = new Validator();
   }
 
-  public void addRestaurant(String noOwner, Restaurant restaurant) {
+  public Restaurant addRestaurant(String noOwner, Restaurant restaurant) {
     if (!isExistOwnerId(noOwner)) {
       restaurantRepository.addOwner(noOwner);
     }
     restaurantRepository.addRestaurant(noOwner, restaurant);
+
+    return restaurant;
   }
 
-  public Restaurant getRestaurantByIdOfOwner(String ownerId, String restaurantId) {
-    return restaurantRepository.getRestaurant(ownerId, restaurantId);
+  public Restaurant getRestaurantByIdOfOwner(String ownerId, String restaurantId)
+      throws NotFoundException {
+
+    Restaurant restaurant = restaurantRepository.getRestaurant(ownerId, restaurantId);
+
+    return restaurant;
   }
 
   public List<Restaurant> getAllRestaurantsOfOwner(String ownerId) {
@@ -47,10 +54,11 @@ public class RestaurantService {
     return false;
   }
 
-  public void isValidOwnerId(String ownerId) throws Exception {
+  public boolean isValidOwnerId(String ownerId) throws Exception {
     if (this.validator.isStringEmpty(ownerId)) {
       throw new MissingParameterException("Missing owner ID.");
     }
+    return true;
   }
 
   public void verifyOwnerId(String ownerId) throws Exception {
@@ -62,12 +70,14 @@ public class RestaurantService {
     }
   }
 
-  public void verifyRestaurantParameter(Restaurant restaurant) throws Exception {
+  public boolean verifyRestaurantParameter(Restaurant restaurant) throws Exception {
 
     if (this.validator.isRestaurantParameterEmpty(restaurant))
       throw new MissingParameterException("Missing restaurant parameter.");
 
     if (!this.validator.isValidRestaurant(restaurant))
       throw new InvalidParameterException("Invalid restaurant parameter.");
+
+    return true;
   }
 }
