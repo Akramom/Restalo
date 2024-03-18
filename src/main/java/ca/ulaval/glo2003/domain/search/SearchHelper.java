da012c5d1d33a467aa5595b8f18774007b817a58
Willy juvenal Konlack foning <willy-juvenal.konlack-foning.1@ulaval.ca>
2024-03-17 20:58:57 -0400

    fix: formatage du code avec spotless:apply

--- src/main/java/ca/ulaval/glo2003/domain/search/SearchHelper.java
+++ src/main/java/ca/ulaval/glo2003/domain/search/SearchHelper.java
@@ -3,24 +3,24 @@ package ca.ulaval.glo2003.domain.search;
 import ca.ulaval.glo2003.domain.entity.Opened;
 import ca.ulaval.glo2003.domain.entity.Restaurant;
 import ca.ulaval.glo2003.util.Util;
-
 import java.time.LocalTime;
 import java.util.List;
-import java.util.Locale;
 import java.util.function.BiPredicate;
-import java.util.function.Function;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class SearchHelper {
 
+  BiPredicate<Restaurant, String> nameFilter =
+      (restaurant, name) -> Util.containsSubstr_toLowerC(restaurant.getName(), name);
+  BiPredicate<Restaurant, LocalTime> openedFromFilter =
+      (restaurant, from) -> restaurant.getHours().getClose().isAfter(from);
+  BiPredicate<Restaurant, LocalTime> openedToFilter =
+      (restaurant, to) ->
+          restaurant.getHours().getClose().equals(to)
+              || restaurant.getHours().getClose().isAfter(to);
 
-  BiPredicate<Restaurant,String> nameFilter=(restaurant , name) ->Util.containsSubstr_toLowerC(restaurant.getName(), name);
-  BiPredicate<Restaurant, LocalTime> openedFromFilter=(restaurant ,from) -> restaurant.getHours().getClose().isAfter(from);
-  BiPredicate<Restaurant, LocalTime> openedToFilter=(restaurant , to) -> restaurant.getHours().getClose().equals(to) || restaurant.getHours().getClose().isAfter(to);
+  public SearchHelper() {}
 
-  public SearchHelper() {
-  }
   public Boolean isSearchDtoOpenedNull(Search search) {
     return search.getOpened() == null;
   }
@@ -36,49 +36,49 @@ public class SearchHelper {
   public List<Restaurant> searchByOpenedHour(List<Restaurant> allRestaurants, Opened opened) {
     if (opened.from() != null && opened.to() == null) {
       return allRestaurants.stream()
-              .filter(restaurant -> openedFromFilter.test(restaurant, opened.from()))
-              .collect(Collectors.toList());
+          .filter(restaurant -> openedFromFilter.test(restaurant, opened.from()))
+          .collect(Collectors.toList());
     }
 
     if (opened.from() == null && opened.to() != null) {
       return allRestaurants.stream()
-              .filter(restaurant -> openedToFilter.test(restaurant, opened.to()))
-              .collect(Collectors.toList());
+          .filter(restaurant -> openedToFilter.test(restaurant, opened.to()))
+          .collect(Collectors.toList());
     }
 
     return allRestaurants.stream()
-            .filter(restaurant -> openedFromFilter.test(restaurant, opened.from()))
-            .filter(restaurant -> openedToFilter.test(restaurant, opened.to()))
-            .collect(Collectors.toList());
+        .filter(restaurant -> openedFromFilter.test(restaurant, opened.from()))
+        .filter(restaurant -> openedToFilter.test(restaurant, opened.to()))
+        .collect(Collectors.toList());
   }
 
   public List<Restaurant> searchByName(List<Restaurant> allRestaurants, String name) {
     return allRestaurants.stream()
-            .filter(restaurant -> nameFilter.test(restaurant, name))
-            .collect(Collectors.toList());
+        .filter(restaurant -> nameFilter.test(restaurant, name))
+        .collect(Collectors.toList());
   }
 
   public List<Restaurant> search(List<Restaurant> allRestaurant, Search search) {
 
     if (search.getOpened().from() == null) {
       return allRestaurant.stream()
-              .filter(restaurant -> nameFilter.test(restaurant, search.getName()))
-              .filter(restaurant -> openedToFilter.test(restaurant, search.getOpened().to()))
-              .collect(Collectors.toList());
+          .filter(restaurant -> nameFilter.test(restaurant, search.getName()))
+          .filter(restaurant -> openedToFilter.test(restaurant, search.getOpened().to()))
+          .collect(Collectors.toList());
     }
 
     if (search.getOpened().to() == null) {
       return allRestaurant.stream()
-              .filter(restaurant -> nameFilter.test(restaurant, search.getName()))
-              .filter(restaurant -> openedFromFilter.test(restaurant, search.getOpened().from()))
-              .collect(Collectors.toList());
+          .filter(restaurant -> nameFilter.test(restaurant, search.getName()))
+          .filter(restaurant -> openedFromFilter.test(restaurant, search.getOpened().from()))
+          .collect(Collectors.toList());
     }
 
     return allRestaurant.stream()
-            .filter(restaurant -> nameFilter.test(restaurant, search.getName()))
-            .filter(restaurant -> openedFromFilter.test(restaurant, search.getOpened().from()))
-            .filter(restaurant -> openedToFilter.test(restaurant, search.getOpened().to()))
-            .collect(Collectors.toList());
+        .filter(restaurant -> nameFilter.test(restaurant, search.getName()))
+        .filter(restaurant -> openedFromFilter.test(restaurant, search.getOpened().from()))
+        .filter(restaurant -> openedToFilter.test(restaurant, search.getOpened().to()))
+        .collect(Collectors.toList());
   }
 
   public List<Restaurant> searchRestaurant(List<Restaurant> allRestaurant, Search search) {
